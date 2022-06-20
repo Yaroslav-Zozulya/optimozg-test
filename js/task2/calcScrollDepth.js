e7bbfef90b6d21d32a5ca1d465805e790b545488
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-06-20 15:46:20 +0300

    added check for 0

--- js/task2/calcScrollDepth.js
+++ js/task2/calcScrollDepth.js
@@ -1,14 +1,19 @@
 export function calcScrollDepth() {
-  /* Вычисляем 1% от общей высоты документа 
-     Высота окна с учётом скролла минус резмер вьюпорта пользователя поделено на 100 
-  */
+  // Получаем значение прокрутки от начала документа
+  const scrollTop = document.documentElement.scrollTop;
+
+  // Проверяем не является ли скрол от верха экрана 0
+  if (scrollTop === 0) {
+    return "0%";
+  }
+
+  // Вычисляем 1% от общей высоты документа
+  //   Высота окна с учётом скролла минус резмер вьюпорта пользователя поделено на 100
+
   const onePercentScroll =
     (document.documentElement.scrollHeight -
       document.documentElement.clientHeight) /
     100;
-
-  // Получаем значение прокрутки от начала документа
-  const scrollTop = document.documentElement.scrollTop;
 
   // Вычисляем в процентах скролл от верха экрана
   const deepScroll = Math.round(scrollTop / onePercentScroll).toString() + "%";
